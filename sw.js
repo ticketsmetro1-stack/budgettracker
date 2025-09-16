a6b8803771c03fa91eac9163f1bc8dbdb13cea1f
BudgetingHacks <ticketsmetro1@gmail.com>
2025-09-16 19:50:15 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'budget-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html', // or the path to your HTML
+  '/sw.js',      // the service worker itself
+];
+
+// Install event – cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate event – cleanup old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+    ))
+  );
+});
+
+// Fetch event – serve from cache if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
